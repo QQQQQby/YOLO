0cce34745bec0cce46bba057fc7bec0fbb4169ab
QQQQQby <yx41_qby@126.com>
2020-06-07 21:36:28 +0800

    Finish the backbone of YOLOv1.

--- YOLOv1/modules.py
+++ YOLOv1/modules.py
@@ -8,7 +8,7 @@ class YOLOv1(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 64, 7, stride=2, padding=3)
-        self.bathc_norm1 = nn.BatchNorm2d(64)
+        self.batch_norm1 = nn.BatchNorm2d(64)
         self.pool1 = nn.MaxPool2d(2, 2)
 
         self.conv2 = nn.Conv2d(64, 192, 3, stride=1, padding=1)
@@ -25,12 +25,39 @@ class YOLOv1(nn.Module):
         self.batch_norm6 = nn.BatchNorm2d(512)
         self.pool3 = nn.MaxPool2d(2, 2)
 
-        # self.conv7 =
+        for i in range(7, 15, 2):
+            setattr(self, 'conv%d' % i, nn.Conv2d(512, 256, 1, stride=1, padding=0))
+            setattr(self, 'batch_norm%d' % i, nn.BatchNorm2d(256))
+            setattr(self, 'conv%d' % (i + 1), nn.Conv2d(256, 512, 3, stride=1, padding=1))
+            setattr(self, 'batch_norm%d' % (i + 1), nn.BatchNorm2d(512))
+        self.conv15 = nn.Conv2d(512, 512, 1, stride=1, padding=0)
+        self.batch_norm15 = nn.BatchNorm2d(512)
+        self.conv16 = nn.Conv2d(512, 1024, 3, stride=1, padding=1)
+        self.batch_norm16 = nn.BatchNorm2d(1024)
+        self.pool4 = nn.MaxPool2d(2, 2)
+
+        for i in range(17, 21, 2):
+            setattr(self, 'conv%d' % i, nn.Conv2d(1024, 512, 1, stride=1, padding=0))
+            setattr(self, 'batch_norm%d' % i, nn.BatchNorm2d(512))
+            setattr(self, 'conv%d' % (i + 1), nn.Conv2d(512, 1024, 3, stride=1, padding=1))
+            setattr(self, 'batch_norm%d' % (i + 1), nn.BatchNorm2d(1024))
+        self.conv21 = nn.Conv2d(1024, 1024, 3, stride=1, padding=1)
+        self.batch_norm21 = nn.BatchNorm2d(1024)
+        self.conv22 = nn.Conv2d(1024, 1024, 3, stride=2, padding=1)
+        self.batch_norm22 = nn.BatchNorm2d(1024)
+        self.conv23 = nn.Conv2d(1024, 1024, 3, stride=1, padding=1)
+        self.batch_norm23 = nn.BatchNorm2d(1024)
+        self.conv24 = nn.Conv2d(1024, 1024, 3, stride=1, padding=1)
+        self.batch_norm24 = nn.BatchNorm2d(1024)
+
+        self.fc1 = nn.Linear(1024 * 7 * 7, 4096, bias=True)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(4096, 30 * 7 * 7, bias=True)
 
     def forward(self, x):
         """3 * 448 * 448"""
         x = self.conv1(x)
-        x = self.bathc_norm1(x)
+        x = self.batch_norm1(x)
         x = F.leaky_relu(x)
         x = self.pool1(x)
         """64 * 112 * 112"""
@@ -39,20 +66,30 @@ class YOLOv1(nn.Module):
         x = F.leaky_relu(x)
         x = self.pool2(x)
         """192 * 56 * 56"""
-        x = self.conv3(x)
-        x = self.batch_norm3(x)
-        x = F.leaky_relu(x)
-        x = self.conv4(x)
-        x = self.batch_norm4(x)
-        x = F.leaky_relu(x)
-        x = self.conv5(x)
-        x = self.batch_norm5(x)
-        x = F.leaky_relu(x)
-        x = self.conv6(x)
-        x = self.batch_norm6(x)
-        x = F.leaky_relu(x)
+        for i in range(3, 7):
+            x = getattr(self, 'conv%d' % i)(x)
+            x = getattr(self, 'batch_norm%d' % i)(x)
+            x = F.leaky_relu(x)
         x = self.pool3(x)
         """512 * 28 * 28"""
-
-
+        for i in range(7, 17):
+            x = getattr(self, 'conv%d' % i)(x)
+            x = getattr(self, 'batch_norm%d' % i)(x)
+            x = F.leaky_relu(x)
+        x = self.pool4(x)
+        """1024 * 14 * 14"""
+        for i in range(17, 25):
+            x = getattr(self, 'conv%d' % i)(x)
+            x = getattr(self, 'batch_norm%d' % i)(x)
+            x = F.leaky_relu(x)
+        """1024 * 7 * 7"""
+        x = x.view(-1, 1024 * 7 * 7)
+        """50176"""
+        x = self.fc1(x)
+        x = self.dropout(x)
+        x = F.leaky_relu(x)
+        """4096"""
+        x = self.fc2(x)
+        """1470"""
+        x = x.view(-1, 30, 7, 7)
         return x
--- YOLOv1/test.py
+++ YOLOv1/test.py
@@ -5,9 +5,10 @@ from torch import nn
 from YOLOv1.modules import YOLOv1
 
 if __name__ == '__main__':
+    if torch.cuda.is_available():
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
     torch.manual_seed(1)
     m = YOLOv1()
-    o = m(torch.rand(1, 3, 448, 448))
-    print(o)
+    o = m(torch.rand(5, 3, 448, 448))
     print(o.shape)
 
