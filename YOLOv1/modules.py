f3c4e42ba1bd84ae24804cc91d32ca6b697f464e
QQQQQby <yx41_qby@126.com>
2020-06-30 19:49:39 +0800

    Add YOLOv1-Tiny.

--- YOLOv1/modules.py
+++ YOLOv1/modules.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -92,6 +93,66 @@ class YOLOv1Backbone(nn.Module):
         x = F.leaky_relu(x, 0.1)
         """4096"""
         x = self.fc2(x)
+        """1470"""
+        x = x.view(-1, 7, 7, 30)
+        """30 * 7 * 7"""
+        return x
+
+
+class YOLOv1TinyBackbone(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3, stride=1, padding=1)
+        self.batch_norm1 = nn.BatchNorm2d(16)
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        self.conv2 = nn.Conv2d(16, 32, 3, stride=1, padding=1)
+        self.batch_norm2 = nn.BatchNorm2d(32)
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        self.conv3 = nn.Conv2d(32, 64, 3, stride=1, padding=1)
+        self.batch_norm3 = nn.BatchNorm2d(64)
+        self.pool3 = nn.MaxPool2d(2, 2)
+
+        self.conv4 = nn.Conv2d(64, 128, 3, stride=1, padding=1)
+        self.batch_norm4 = nn.BatchNorm2d(128)
+        self.pool4 = nn.MaxPool2d(2, 2)
+
+        self.conv5 = nn.Conv2d(128, 256, 3, stride=1, padding=1)
+        self.batch_norm5 = nn.BatchNorm2d(256)
+        self.pool5 = nn.MaxPool2d(2, 2)
+
+        self.conv6 = nn.Conv2d(256, 512, 3, stride=1, padding=1)
+        self.batch_norm6 = nn.BatchNorm2d(512)
+        self.pool6 = nn.MaxPool2d(2, 2)
+
+        self.conv7 = nn.Conv2d(512, 1024, 3, stride=1, padding=1)
+        self.batch_norm7 = nn.BatchNorm2d(1024)
+
+        self.conv8 = nn.Conv2d(1024, 256, 3, stride=1, padding=1)
+        self.batch_norm8 = nn.BatchNorm2d(256)
+
+        self.fc1 = nn.Linear(256 * 7 * 7, 7 * 7 * 30, bias=True)
+
+    def forward(self, x):
+        """448 * 448 * 3"""
+        x = x.permute(0, 3, 1, 2).float()
+        """3 * 448 * 448"""
+        for i in range(1, 7):
+            x = getattr(self, 'conv%d' % i)(x)
+            x = getattr(self, 'batch_norm%d' % i)(x)
+            x = F.leaky_relu(x, 0.1)
+            x = getattr(self, 'pool%d' % i)(x)
+        """512 * 28 * 28"""
+        for i in range(7, 9):
+            x = getattr(self, 'conv%d' % i)(x)
+            x = getattr(self, 'batch_norm%d' % i)(x)
+            x = F.leaky_relu(x, 0.1)
+        """256 * 7 * 7"""
+        x = x.view(-1, 256 * 7 * 7)
+        """25088"""
+        x = self.fc1(x)
+        x = F.leaky_relu(x, 0.1)
         """1470"""
         x = x.view(-1, 7, 7, 30)
         """30 * 7 * 7"""
