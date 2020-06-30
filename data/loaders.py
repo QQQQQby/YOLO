ededeacf9ac0518ad9710961ac9d911fcd11e048
QQQQQby <yx41_qby@126.com>
2020-06-30 19:41:00 +0800

    Use multiprocessing to read data.

--- data/loaders.py
+++ data/loaders.py
@@ -1,9 +1,13 @@
+# coding: utf-8
+
 import os
 from xml.dom import minidom
 import cv2
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
+from multiprocessing import Pool
+from p_tqdm import p_map
 
 
 class DataLoader:
@@ -34,7 +38,7 @@ class DataLoader:
 
 
 class VOC2012Loader(DataLoader):
-    def __init__(self, path, train_prop=0.8):
+    def __init__(self, path, train_prop=0.8, num_processes=4):
         super(VOC2012Loader, self).__init__(path)
         file_names = os.listdir(os.path.join(self.get_path(), "VOC2012", "train", "JPEGImages"))
         file_names = [name.replace('.jpg', '') for name in file_names]
@@ -45,24 +49,80 @@ class VOC2012Loader(DataLoader):
         self.train_file_names = file_names[:split_index]
         self.dev_file_names = file_names[split_index:]
 
+        self.num_processes = num_processes
+
     def get_data_train(self):
+        """多进程读取"""
+        image_object_paths = []
+        for name in self.train_file_names:
+            image_object_paths.append(
+                (os.path.join(self.get_path(), "VOC2012", "train", "JPEGImages", name + '.jpg'),
+                 os.path.join(self.get_path(), "VOC2012", "train", "Annotations", name + '.xml'))
+            )
         res = []
-        for name in tqdm(self.train_file_names):
-            image_path = os.path.join(self.get_path(), "VOC2012", "train", "JPEGImages", name + '.jpg')
-            object_path = os.path.join(self.get_path(), "VOC2012", "train", "Annotations", name + '.xml')
-            image = self.read_image_array(image_path)
-            objects = self.read_objects(object_path)
-            res.append([image, objects])
+        process_bar = tqdm(range(len(self.train_file_names)))
+        with Pool(self.num_processes) as p:
+            for data in p.imap(self.read_image_and_objects, image_object_paths):
+                res.append(data)
+                process_bar.update()
+            p.close()
+        process_bar.close()
         return res
 
+        # res = [None for i in range(len(self.dev_file_names))]
+        #
+        # processes = []
+        # length = int(float(len(self.dev_file_names)) / float(self.num_processes))
+        # indices = [int(round(i * length)) for i in range(self.num_processes)]
+        # indices.append(len(self.dev_file_names))
+        #
+        # train_file_name_sublists = [self.dev_file_names[indices[i]:indices[i + 1]]
+        #                             for i in range(self.num_processes)]
+        #
+        # for i in range(self.num_processes):
+        #     image_paths = []
+        #     object_paths = []
+        #     for j in range(len(train_file_name_sublists[i])):
+        #         image_paths.append(os.path.join(
+        #             self.get_path(), "VOC2012", "train", "JPEGImages",
+        #             train_file_name_sublists[i][j] + '.jpg'
+        #         ))
+        #         object_paths.append(os.path.join(
+        #             self.get_path(), "VOC2012", "train", "Annotations",
+        #             train_file_name_sublists[i][j] + '.xml'
+        #         ))
+        #     processes.append(Process(
+        #         target=self.read_data_to_list,
+        #         args=(res, indices[i], indices[i + 1], image_paths, object_paths))
+        #     )
+        # for p in processes:
+        #     p.start()
+        # for p in processes:
+        #     p.join()
+        # return res
+
+    # def read_data_to_list(self, res, start, end, image_paths, object_paths):
+    #     for path_i, list_i in enumerate(range(start, end)):
+    #         res[list_i] = (
+    #             self.read_image_array(image_paths[path_i]),
+    #             self.read_objects(object_paths[path_i])
+    #         )
+
     def get_data_dev(self):
+        image_object_paths = []
+        for name in self.dev_file_names:
+            image_object_paths.append(
+                (os.path.join(self.get_path(), "VOC2012", "train", "JPEGImages", name + '.jpg'),
+                 os.path.join(self.get_path(), "VOC2012", "train", "Annotations", name + '.xml'))
+            )
         res = []
-        for name in tqdm(self.dev_file_names):
-            image_path = os.path.join(self.get_path(), "VOC2012", "train", "JPEGImages", name + '.jpg')
-            object_path = os.path.join(self.get_path(), "VOC2012", "train", "Annotations", name + '.xml')
-            image = self.read_image_array(image_path)
-            objects = self.read_objects(object_path)
-            res.append([image, objects])
+        process_bar = tqdm(range(len(self.dev_file_names)))
+        with Pool(self.num_processes) as p:
+            for data in p.imap(self.read_image_and_objects, image_object_paths):
+                res.append(data)
+                process_bar.update()
+            p.close()
+        process_bar.close()
         return res
 
     def get_data_test(self):
@@ -73,6 +133,12 @@ class VOC2012Loader(DataLoader):
                 "bird", "cat", "cow", "dog", "horse", "sheep",
                 "aeroplane", "bicycle", "boat", "bus", "car", "motorbike", "train",
                 "bottle", "chair", "diningtable", "pottedplant", "sofa", "tvmonitor"]
+
+    def read_image_and_objects(self, image_object_path):
+        return (
+            self.read_image_array(image_object_path[0]),
+            self.read_objects(image_object_path[1])
+        )
 
     @classmethod
     def read_image_array(self, path):
