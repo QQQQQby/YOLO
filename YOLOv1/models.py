Fix some little issues. Finish calculation of precision and recall.

YOLOv1/models.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from util.functions import iou
+from util.metrics import get_precision_and_recall, get_AP
 
 import torch
 from torch import nn, optim
@@ -20,7 +21,7 @@ class YOLOv1:
             lambda_noobj,
             clip_max_norm=None,
             score_threshold=0.5,
-            iou_threshold_pred=0.5,
+            iou_threshold_pred=0.2,
             iou_thresholds_mmAP=None
     ):
         self.backbone = backbone
@@ -76,10 +77,10 @@ class YOLOv1:
                     pred_coord_dict[w_label] = output_dict[w_label][data_id, row, col] * 448
                     pred_coord_dict[h_label] = output_dict[h_label][data_id, row, col] * 448
                     """计算gt与dt的IOU"""
-                    ious.append(iou((float(pred_coord_dict[x_label]),
-                                     float(pred_coord_dict[y_label]),
-                                     float(pred_coord_dict[w_label]),
-                                     float(pred_coord_dict[h_label])),
+                    ious.append(iou((int(pred_coord_dict[x_label]),
+                                     int(pred_coord_dict[y_label]),
+                                     int(pred_coord_dict[w_label]),
+                                     int(pred_coord_dict[h_label])),
                                     (true_dict['x'],
                                      true_dict['y'],
                                      true_dict['w'],
@@ -156,7 +157,8 @@ class YOLOv1:
                 for c_i in range(len(candidates) - 1):
                     if candidates[c_i][2] > 0:
                         for c_j in range(c_i + 1, len(candidates)):
-                            if iou(candidates[c_i][3:], candidates[c_j][3:], 448, 448) > self.iou_threshold_pred:
+                            if iou(tuple(candidates[c_i][3:]), tuple(candidates[c_j][3:]), 448, 448) \
+                                    > self.iou_threshold_pred:
                                 candidates[c_j][2] = -1
                 for c_i in range(len(candidates)):
                     if candidates[c_i][2] > 0:
@@ -165,17 +167,43 @@ class YOLOv1:
                             "y": candidates[c_i][4],
                             "w": candidates[c_i][5],
                             "h": candidates[c_i][6],
-                            "name": category
+                            "name": category,
+                            "score": candidates[c_i][2]
                         })
         # print(results)
         return results
 
-    # def get_mmAP(self, batch):
-    #     output_dict = self.get_output_dict([data[0] for data in batch])
-    #     results = []
-    #     for data_id in range(len(batch)):
+    def get_mmAP(self, batch):
+        pred_results = self.predict([data[0] for data in batch])
+        results = []
+        for data_id in range(len(batch)):
+            dt_list = pred_results[data_id]
+            dt_dict = {}
+            for category in self.labels:
+                dt_dict[category] = []
+            for dt in dt_list:
+                dt_dict[dt["name"]].append(dt)
 
+            gt_list = batch[data_id][1]
+            gt_dict = {}
+            for category in self.labels:
+                gt_dict[category] = []
+            for gt in gt_list:
+                gt_dict[gt["name"]].append(gt)
 
+            for iou_threshold in self.iou_thresholds_mmAP:
+                for category in self.labels:
+                    """"""
+                    dts = dt_dict[category]
+                    gts = gt_dict[category]
+                    dts.sort(key=lambda x: -x["score"])  # 将所有的dt按分数从高到低排序
+                    precisions = []
+                    recalls = []
+                    for n in range(1, len(dts)):
+                        p, r = get_precision_and_recall(dts[:n], gts, iou_threshold)
+                        precisions.append(p)
+                        recalls.append(r)
+                    AP = get_AP(precisions, recalls)
 
     def get_output_dict(self, images):
         output_tensor = self.backbone(torch.from_numpy(np.array(images)).

util/functions.py
@@ -2,22 +2,31 @@
 
 import cv2
 import numpy as np
+from typing import List, Tuple, Dict
 
 
-def iou(bbox0, bbox1, max_width, max_height):
-    upper_left0 = (round(bbox0[0] - bbox0[2] / 2), round(bbox0[1] - bbox0[3] / 2))
-    upper_left1 = (round(bbox1[0] - bbox1[2] / 2), round(bbox1[1] - bbox1[3] / 2))
+def iou(bbox0: Tuple[int, int, int, int], bbox1: Tuple[int, int, int, int], max_width: int, max_height: int) -> float:
+    x0, y0, w0, h0 = bbox0[:4]
+    x1, y1, w1, h1 = bbox1[:4]
+    upper_left0 = (round(x0 - w0 / 2), round(y0 - h0 / 2))
+    upper_left1 = (round(x1 - w1 / 2), round(y1 - h1 / 2))
+    lower_right0 = (round(x0 + w0 / 2), round(y0 + h0 / 2))
+    lower_right1 = (round(x1 + w1 / 2), round(y1 + h1 / 2))
     inter_upper_left = (max(upper_left0[0], upper_left1[0]), max(upper_left0[1], upper_left1[1]))
-    lower_right0 = (round(bbox0[0] + bbox0[2] / 2), round(bbox0[1] + bbox0[3] / 2))
-    lower_right1 = (round(bbox1[0] + bbox1[2] / 2), round(bbox1[1] + bbox1[3] / 2))
     inter_lower_right = (min(lower_right0[0], lower_right1[0]), min(lower_right0[1], lower_right1[1]))
     inter_area = 0 if inter_upper_left[0] > inter_lower_right[0] or inter_upper_left[1] > inter_lower_right[1] \
         else (inter_lower_right[0] - inter_upper_left[0] + 1) * (inter_lower_right[1] - inter_upper_left[1] + 1)
-    outer_area = bbox0[2] * bbox0[3] + bbox1[2] * bbox1[3] - inter_area
+    outer_area = w0 * h0 + w1 * h1 - inter_area
     return inter_area / outer_area if outer_area != 0 else 0
 
 
 def show_objects(image_array: np.ndarray, objects, color_dict):
+    image = draw_image(image_array, objects, color_dict)
+    cv2.imshow('Object Detection', image)
+    cv2.waitKey(750)
+
+
+def draw_image(image_array: np.ndarray, objects, color_dict):
     image = image_array[:, :, ::-1].copy()
     for o in objects:
         upper_left = (int(o['x'] - o['w'] / 2), int(o['y'] - o['h'] / 2))
@@ -34,7 +43,6 @@ def show_objects(image_array: np.ndarray, objects, color_dict):
 
         upper_left = (upper_left[0] + 2, max(upper_left[1] - 4, 12))
         cv2.putText(image, o['name'], upper_left, cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255)[::-1], 1)
-    cv2.imshow('Object Detection', image)
-    cv2.waitKey(750)
+    return image
 
 # def AP()