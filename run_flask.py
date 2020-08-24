872b78f3f87bf60aba14490b4b3fc21d03be54c0
QQQQQby <yx41_qby@126.com>
2020-08-24 14:11:08 +0800

    Add a thread to limit GPU usage.

--- run_flask.py
+++ run_flask.py
@@ -1,27 +1,33 @@
+# coding: utf-8
+
 from gevent import pywsgi
 from flask import Flask, request
+import threading
+import torch
+import time
 import base64
 import os
 
 from util.loaders import read_classes, get_color_dict, read_anchors
 from models import YOLO
 
-app = Flask(__name__)
-
 
 def after_request(response):
-    # JS前端跨域支持
     response.headers['Cache-Control'] = 'no-cache'
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
 
+app = Flask(__name__)
 app.after_request(after_request)
 
 
-# 这里默认的是get请求方式
 @app.route('/detection/', methods=['POST'])
 def upload():
+    global model_lock, model, model_use_time
+    model_lock.acquire()
+    if model.backbone is None:
+        model.backbone = torch.load("./models/yolov3.pth").cuda()
     f = request.files["image"]
     print("Receive image:", f.filename)
     in_path = os.path.join("./data/images", f.filename)
@@ -36,9 +42,29 @@ def upload():
             do_show=False,
             output_path=out_path
         )
+    model_use_time = time.time()
+    model_lock.release()
     with open(out_path, 'rb') as f:
         image = base64.b64encode(f.read())
     return image
+
+
+def cuda_memory_control(max_time, time_gap):
+    global model_lock, model, model_use_time
+    while True:
+        model_lock.acquire()
+        if time.time() - model_use_time >= max_time and model.backbone is not None:
+            model.backbone = None
+            torch.cuda.empty_cache()
+            print("Cache cleaned.")
+        model_lock.release()
+        time.sleep(time_gap)
+
+
+def run_server():
+    server = pywsgi.WSGIServer(('0.0.0.0', 34560), app, log=app.logger)
+    server.serve_forever()
+    # app.run(threaded=False)
 
 
 if __name__ == '__main__':
@@ -49,7 +75,9 @@ if __name__ == '__main__':
                  model_load_path="./models/yolov3.pth",
                  anchors=anchors,
                  device_ids="0")
-    server = pywsgi.WSGIServer(('0.0.0.0', 34560), app)
-    # server = make_server('0.0.0.0', 34556, app)
-    server.serve_forever()
-    app.run(threaded=False)
+
+    model_use_time = time.time()
+    model_lock = threading.Lock()
+    threading.Thread(target=cuda_memory_control, args=(60, 30)).start()
+    threading.Thread(target=run_server).start()
+    print("Server is successfully started.")
